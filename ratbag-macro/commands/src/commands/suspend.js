d33a150f90f8eeb401e506cdea7777d739200cb8
clabroche <corentinlabroche@gmail.com>
2023-03-07 03:33:48 +0100

    fix(ratbag): support for systemctl job canceling

--- ratbag-macro/commands/src/commands/suspend.js
+++ ratbag-macro/commands/src/commands/suspend.js
@@ -1,5 +1,7 @@
 const { execSync } = require('child_process')
 
 module.exports.suspend = async function () {
+  const job = Number.parseInt(execSync('systemctl list-jobs | grep suspend.target' , {encoding: 'utf-8'}).trim())
+  if (!Number.isNaN(job)) execSync(`systemctl cancel ${job}`)
   execSync('systemctl suspend')
 }
